99133e48a57f9929b1f154b51e9eebfc29160f18
duhan-kim <114125957+duhan-kim@users.noreply.github.com>
2022-10-05 11:58:51 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,24 +1,24 @@
 radio.onReceivedString(function (receivedString) {
-    signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    신호강도 = radio.receivedPacket(RadioPacketProperty.SignalStrength)
     led.plotBarGraph(
-    Math.map(signal, -95, -42, 0, 9),
+    Math.map(신호강도, -95, -42, 0, 9),
     9
     )
-    if (0 <= radio.receivedPacket(RadioPacketProperty.SignalStrength)) {
-        music.playTone(932, music.beat(BeatFraction.Breve))
-    } else if (0 < radio.receivedPacket(RadioPacketProperty.SignalStrength) && 1 <= radio.receivedPacket(RadioPacketProperty.SignalStrength)) {
-        music.playTone(932, music.beat(BeatFraction.Double))
-    } else if (1 < radio.receivedPacket(RadioPacketProperty.SignalStrength) && 2 <= radio.receivedPacket(RadioPacketProperty.SignalStrength)) {
-        music.playTone(932, music.beat(BeatFraction.Whole))
-    } else if (2 < radio.receivedPacket(RadioPacketProperty.SignalStrength) && 3 <= radio.receivedPacket(RadioPacketProperty.SignalStrength)) {
-        music.playTone(932, music.beat(BeatFraction.Half))
-    } else if (3 < radio.receivedPacket(RadioPacketProperty.SignalStrength) && 4 <= radio.receivedPacket(RadioPacketProperty.SignalStrength)) {
-        music.playTone(932, music.beat(BeatFraction.Quarter))
-    } else if (4 < radio.receivedPacket(RadioPacketProperty.SignalStrength) && 5 <= radio.receivedPacket(RadioPacketProperty.SignalStrength)) {
-        music.playTone(932, music.beat(BeatFraction.Eighth))
-    } else if (6 < radio.receivedPacket(RadioPacketProperty.SignalStrength) && 7 <= radio.receivedPacket(RadioPacketProperty.SignalStrength)) {
-        music.playTone(932, music.beat(BeatFraction.Sixteenth))
+    if (0 == radio.receivedPacket(RadioPacketProperty.SignalStrength)) {
+    	
+    } else if (0 <= radio.receivedPacket(RadioPacketProperty.SignalStrength) || 1 > radio.receivedPacket(RadioPacketProperty.SignalStrength)) {
+        music.playTone(523, music.beat(BeatFraction.Double))
+    } else if (1 <= radio.receivedPacket(RadioPacketProperty.SignalStrength) || 2 > radio.receivedPacket(RadioPacketProperty.SignalStrength)) {
+        music.playTone(587, music.beat(BeatFraction.Whole))
+    } else if (2 <= radio.receivedPacket(RadioPacketProperty.SignalStrength) || 3 > radio.receivedPacket(RadioPacketProperty.SignalStrength)) {
+        music.playTone(659, music.beat(BeatFraction.Half))
+    } else if (3 <= radio.receivedPacket(RadioPacketProperty.SignalStrength) || 4 > radio.receivedPacket(RadioPacketProperty.SignalStrength)) {
+        music.playTone(698, music.beat(BeatFraction.Quarter))
+    } else if (4 <= radio.receivedPacket(RadioPacketProperty.SignalStrength) || 5 > radio.receivedPacket(RadioPacketProperty.SignalStrength)) {
+        music.playTone(784, music.beat(BeatFraction.Eighth))
+    } else if (6 <= radio.receivedPacket(RadioPacketProperty.SignalStrength) || 7 > radio.receivedPacket(RadioPacketProperty.SignalStrength)) {
+        music.playTone(880, music.beat(BeatFraction.Sixteenth))
     }
 })
-let signal = 0
+let 신호강도 = 0
 radio.setGroup(1)
